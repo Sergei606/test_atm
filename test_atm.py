0b29e7b8652c119a293c686a29b6b95b69db0762
Sergei <kotelevecsergej5@gmail.com>
2020-06-17 14:05:47 +0300

    added attributes

--- test_atm.py
+++ test_atm.py
@@ -4,7 +4,9 @@ from features import *
 class TestAtm(unittest.TestCase):
     def setUp(self):
         self.atm = Atm()
-        self.pin = self.atm.enter_pin(777)
+        self.pin = 777
+        self.attempts = 2
+        self.balance = 10000
 
     def test_rise_money(self):
         self.assertTrue(self.atm.rise_money(1000))
@@ -16,16 +18,26 @@ class TestAtm(unittest.TestCase):
         self.assertTrue(True, self.pin)
 
     def test_check_balance(self):
+        self.atm.enter_pin(self.pin)
         self.assertEqual(10000, self.atm.check_balance())
 
     def test_incorrect_balance(self):
-        self.assertNotEqual(99999, self.atm.check_balance())
+        self.atm.enter_pin(self.pin)
+        self.assertNotEqual(9999, self.atm.check_balance())
 
     def test_incorrect_pin(self):
         self.assertNotEqual(111, self.pin)
 
     def test_get_money(self):
+        self.atm.enter_pin(self.pin)
         self.assertEqual(5000, self.atm.get_money(5000))
-        
+
+    def test_attempts(self):
+        self.atm.enter_pin(777)
+        self.assertTrue(self.attempts)
+
+
+
+
 
 
